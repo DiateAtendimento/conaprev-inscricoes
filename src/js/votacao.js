99aa3ebfa9faa0481f8a5818a4de7a31a0138794
Ricardo Andre Ximenes Dos Santos <andre.ximenes@previdencia.gov.br>
2026-02-10 09:08:17 -0300

    ajustes mod

--- src/js/votacao.js
+++ src/js/votacao.js
@@ -768,38 +768,6 @@
       SE: 'Sergipe',
       TO: 'Tocantins',
     };
-    const STATE_FLAG_DIR_PUBLIC = STATE_FLAG_DIR;
-    const ASSOC_IMAGE_DIR_PUBLIC = ASSOC_IMAGE_DIR;
-
-    const UF_NAMES = {
-      AC: 'Acre',
-      AL: 'Alagoas',
-      AP: 'Amapa',
-      AM: 'Amazonas',
-      BA: 'Bahia',
-      CE: 'Ceara',
-      DF: 'Distrito Federal',
-      ES: 'Espirito Santo',
-      GO: 'Goias',
-      MA: 'Maranhao',
-      MT: 'Mato Grosso',
-      MS: 'Mato Grosso do Sul',
-      MG: 'Minas Gerais',
-      PA: 'Para',
-      PB: 'Paraiba',
-      PR: 'Parana',
-      PE: 'Pernambuco',
-      PI: 'Piaui',
-      RJ: 'Rio de Janeiro',
-      RN: 'Rio Grande do Norte',
-      RS: 'Rio Grande do Sul',
-      RO: 'Rondonia',
-      RR: 'Roraima',
-      SC: 'Santa Catarina',
-      SP: 'Sao Paulo',
-      SE: 'Sergipe',
-      TO: 'Tocantins',
-    };
     let cityDataPromise = null;
     let assocListPromise = null;
 
